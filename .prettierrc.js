9018b7a79eae12b245eaf3eae26727b79ab66576
DanilovG103 <danilov2706@gmail.com>
2022-02-04 12:38:47 +0400

    fix: linting

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,11 @@
 module.exports = {
-  bracketSpacing: false,
+  bracketSpacing: true,
   jsxBracketSameLine: true,
   singleQuote: true,
   trailingComma: 'all',
+  tabWidth: 2,
+  printWidth: 80,
+  semi: false,
   arrowParens: 'avoid',
-};
+  endOfLine: 'auto'
+}
---
+++ src/api/index.ts
@@ -0,0 +1,1 @@
+export const getPhoto = async (props: string) => {}
