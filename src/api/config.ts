ac7b4db538ea78d059af344f3926ab556218ea4f
DanilovG103 <danilov2706@gmail.com>
2022-02-07 20:34:41 +0400

    feat: add queue

--- src/api/config.ts
+++ src/api/config.ts
@@ -2,5 +2,5 @@ export const getPhoto = async (props: string) => {
   const term = props.substring(0, props.length - 4).replace(' ', '+')
   const response = await fetch(`https://itunes.apple.com/search?term=${term}`)
   const result = await response.json()
-  return result.results[0].artworkUrl100
+  return result.resultsCount > 0 ? result.results[0].artworkUrl100 : ''
 }
--- src/components/MusicList.tsx
+++ src/components/MusicList.tsx
@@ -1,13 +1,19 @@
 import React, { useState, useEffect } from 'react'
-import { FlatList } from 'react-native'
+import { FlatList, Image } from 'react-native'
 import * as RNFS from 'react-native-fs'
-import TrackPlayer, { Capability } from 'react-native-track-player'
+import TrackPlayer, {
+  Capability,
+  Event,
+  RepeatMode,
+  Track,
+} from 'react-native-track-player'
 import { getPhoto } from 'src/api/config'
 import styled from 'styled-components/native'
 
 const MusicBlock = styled.TouchableOpacity`
   width: 100%;
   padding: 10px 15px;
+  flex-direction: row;
 `
 
 const Text = styled.Text`
@@ -15,8 +21,13 @@ const Text = styled.Text`
   color: white;
 `
 
+interface Props {
+  item: Track
+}
+
 export const MusicList = () => {
   const [files, setFiles] = useState<RNFS.ReadDirItem[]>([])
+  const [tracks, setTracks] = useState<Track[]>([])
 
   useEffect(() => {
     RNFS.readDir(`${RNFS.ExternalStorageDirectoryPath}/Music`)
@@ -27,7 +38,31 @@ export const MusicList = () => {
       .catch(err => console.log(err))
   }, [])
 
-  const start = async (file: RNFS.ReadDirItem) => {
+  useEffect(() => {
+    const fetchAsync = async () => {
+      let result = []
+      for (const file of files) {
+        const artwork = await getPhoto(file.name)
+        result.push({
+          id: file.name,
+          url: 'file://' + file.path,
+          title: file.name,
+          artwork,
+        })
+        setTracks(result)
+      }
+    }
+
+    if (files.length > 0) {
+      fetchAsync()
+    }
+  }, [files])
+
+  console.log(tracks.length)
+
+  const start = async (track: Track) => {
+    await TrackPlayer.setupPlayer({ maxCacheSize: 1000 })
+
     await TrackPlayer.updateOptions({
       capabilities: [
         Capability.Pause,
@@ -38,26 +73,49 @@ export const MusicList = () => {
       stopWithApp: true,
     })
 
-    await TrackPlayer.setupPlayer()
+    await TrackPlayer.add([
+      {
+        id: track.title,
+        url: track.url,
+        title: track.title,
+        artwork: track.artwork,
+      },
+      ...tracks,
+    ])
 
-    await TrackPlayer.add({
-      id: 'trackId',
-      url: 'file://' + file.path,
-      title: file.name,
-      artwork: await getPhoto(file.name),
-    })
+    TrackPlayer.setRepeatMode(RepeatMode.Queue)
 
     await TrackPlayer.play()
   }
 
-  return (
-    <FlatList
-      data={files}
-      renderItem={({ item }) => (
-        <MusicBlock onPress={() => start(item)}>
-          <Text>{item.name.replace('.mp3', '')}</Text>
-        </MusicBlock>
-      )}
-    />
+  TrackPlayer.addEventListener(
+    Event.RemotePause,
+    async () => await TrackPlayer.pause(),
   )
+
+  TrackPlayer.addEventListener(
+    Event.RemotePlay,
+    async () => await TrackPlayer.play(),
+  )
+
+  TrackPlayer.addEventListener(
+    Event.RemoteNext,
+    async () => await TrackPlayer.skipToNext(),
+  )
+
+  TrackPlayer.addEventListener(
+    Event.RemotePrevious,
+    async () => await TrackPlayer.skipToPrevious(),
+  )
+
+  const renderItem = ({ item }: Props) => {
+    return (
+      <MusicBlock onPress={() => start(item)}>
+        <Image width={45} height={45} source={{ uri: item.artwork }} />
+        <Text>{item.title}</Text>
+      </MusicBlock>
+    )
+  }
+
+  return <FlatList data={tracks} renderItem={renderItem} />
 }
